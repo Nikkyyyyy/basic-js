2e33b114afdec84e6cb750187969e7fbf6a559f4
Nikita Banskota <nikitabanskota2@gmail.com>
2023-09-25 16:01:31 +0545

    let,var,const

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 console.log("Hello from index.js")
+
+//ES5
+
+var  person="Nikita"
+person= "Nikita"
+
+console.log(person);
+
+
+//Es6 
+
+// const: constant (doesnot change)
+
+// let : changable variable
+
+
+
+let people = "hari"
+people="rita"
+
+console.log(people);
+
+
+const numberDefine= 3.144
+console.log(numberDefine);
+
+const isValid = true; //Bollen
+console.log(isValid);
+
+
