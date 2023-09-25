0e6202602048ab171e4c2ffe0c32c0fdf54442fa
Nikita Banskota <nikitabanskota2@gmail.com>
2023-09-25 16:32:11 +0545

    browser based interaction

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log("Hello from index.js")
 
 //ES5
-
+/*
 var  person="Nikita"
 person= "Nikita"
 
 console.log(person);
-
+*/
 
 //Es6 
 
@@ -15,7 +15,7 @@ console.log(person);
 // let : changable variable
 
 
-
+/*
 let people = "hari"
 people="rita"
 
@@ -27,5 +27,23 @@ console.log(numberDefine);
 
 const isValid = true; //Bollen
 console.log(isValid);
+*/
+
+//Browser based interaction
+
+const personName = prompt("What is your name?")
+console.log(personName);
+
+const isAbove18 = confirm("Your age is above 18? ")
+console.log(isAbove18);
+
+alert(personName, isAbove18);
 
 
+const isOfAge = isAbove18 ? "above" : "before"
+const text = "My name is "+ personName +" and I am "+ isOfAge + " 18 years";
+alert(text);
+console.log(text);
+
+//WRITE A JS CODE THAT ASK FOR USERNAME AND PASSWORD WITH CONFIRM PASSWORD,
+// IF THE USERNAME AND PASSWORD MATCHES ALERT: YOU HAVE SUCCESSFULLY LOGED In ELSE ALERT : USERNAME AND PASSWORD IS INCORRECT
