d029a94082c20cea2e4232d5f387dd12869020b3
Nikita Banskota <nikitabanskota2@gmail.com>
2023-10-01 14:57:42 +0545

    user name form

--- index.js
+++ index.js
@@ -31,19 +31,43 @@ console.log(isValid);
 
 //Browser based interaction
 
-const personName = prompt("What is your name?")
-console.log(personName);
+// const personName = prompt("What is your name?")
+// console.log(personName);
 
-const isAbove18 = confirm("Your age is above 18? ")
-console.log(isAbove18);
+// const isAbove18 = confirm("Your age is above 18? ")
+// console.log(isAbove18);
 
-alert(personName, isAbove18);
+// alert(personName, isAbove18);
 
 
-const isOfAge = isAbove18 ? "above" : "before"
-const text = "My name is "+ personName +" and I am "+ isOfAge + " 18 years";
-alert(text);
-console.log(text);
+// const isOfAge = isAbove18 ? "above" : "before"
+// const text = "My name is "+ personName +" and I am "+ isOfAge + " 18 years";
+// alert(text);
+// console.log(text);
 
 //WRITE A JS CODE THAT ASK FOR USERNAME AND PASSWORD WITH CONFIRM PASSWORD,
 // IF THE USERNAME AND PASSWORD MATCHES ALERT: YOU HAVE SUCCESSFULLY LOGED In ELSE ALERT : USERNAME AND PASSWORD IS INCORRECT
+
+
+document.querySelector('.button').onclick = function(){
+    var password = document.querySelector('.password').value,
+    confirmPassword = document.querySelector('.confirmPassword').value;
+
+    if(password == ""){
+        alert("Field cannot be empty.");
+    }
+    else if(password != confirmPassword){
+        alert("Password didn't match. Try Again!!!")
+        return false
+    }
+    else if(password == confirmPassword){
+        alert("Password Match!")
+        
+    }
+    return true
+
+
+
+
+}
+
